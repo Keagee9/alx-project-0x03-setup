64c94a2093bd885081dbd37fc2cedfa130638a6f
Keagee9 <Kingsleyfrancis.kalu@gmail.com>
2025-08-02 04:55:59 +0100

    week5

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -5,10 +5,12 @@ interface PageRouteProps {
   pageRoute: string
 }
 
+const router = useRouter();
+
 export default function Home() {
   const router = useRouter()
 
-  // Imeperative routing with useRouter
+  // Imperative routing with useRouter
   const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
     router.push(pageRoute, undefined, { shallow: false})
   }
